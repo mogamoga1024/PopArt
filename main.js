9fe5596ed75e6c01152d264d3b1dac405cdd5af3
mogamoga <mogamoga.g1thu6@gmail.com>
2023-09-12 00:07:16 +0900

    hsl

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ ImageTracer.imageToSVG(
     "野獣先輩.png",
     function (strOriSvg) {
         // console.log(strOriSvg);
-        let strSvg = strOriSvg.replaceAll("55,45,50", "255,0,0"); // 髪、服など
-        strSvg = strSvg.replaceAll("115,79,71", "0,255,0"); // 影
-        strSvg = strSvg.replaceAll("166,114,102", "0,0,255"); // 皮膚
-        strSvg = strSvg.replaceAll("254,253,253", "255,255,255"); // 背景
+        let strSvg = strOriSvg.replaceAll("rgb(55,45,50)", "rgb(255,0,0)"); // 髪、服など
+        strSvg = strSvg.replaceAll("rgb(115,79,71)", "rgb(0,255,0)"); // 影
+        strSvg = strSvg.replaceAll("rgb(166,114,102)", "rgb(0,0,255)"); // 皮膚
+        strSvg = strSvg.replaceAll("rgb(254,253,253)", "rgb(255,255,255)"); // 背景
 
         console.log(strSvg);
         ImageTracer.appendSVGString(strSvg, "svg");
