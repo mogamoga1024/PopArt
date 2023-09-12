b7bb67fae4b5ca1a4b86b88d774984d1efd4b95a
mogamoga <mogamoga.g1thu6@gmail.com>
2023-09-12 18:14:08 +0900

    ２人の約束が口癖で終わらぬように

--- main.js
+++ main.js
@@ -4,44 +4,6 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createRandomColorStrSVG(strOriSvg, palette) {
-    let strSvg = strOriSvg;
-    let firstHue = 0;
-    let secondHue = 0;
-    for (let i = 0; i < palette.length; i++) {
-        const color = palette[i];
-        const src = `rgb(${color.r},${color.g},${color.b})`;
-        let hue = randomInt(0, 360 - 1);
-        // const saturation = 100 + randomInt(-10, 10) + randomInt(-10, 10) + randomInt(-10, 10);
-        const saturation = 100;
-        // let lightness = 50 + randomInt(-25, 25) + randomInt(-25, 25);
-        let lightness = 50;
-        for (let i = 0; i < 25; i++) {
-            lightness += randomInt(-1, 1);
-        }
-
-        if (i === 0) {
-            firstHue = hue;
-        }
-        else if (i === 1) {
-            hue = firstHue + randomInt(-135, 135);
-            lightness = 40;
-            secondHue = hue;
-        }
-        else if (i === 2) {
-            hue = secondHue + 30;
-            lightness = 60;
-        }
-        else if (i === palette.length - 1) {
-            hue = firstHue + 180 + randomInt(-20, 20);
-        }
-
-        const dst = `hsl(${hue} ${saturation}% ${lightness}%)`;
-        strSvg = strSvg.replaceAll(src, dst);
-    }
-    return strSvg;
-}
-
 const options = {
     // ltres: 1,
     // qtres: 1,
@@ -60,17 +22,29 @@ const options = {
 ImageTracer.imageToTracedata(
     "野獣先輩.png",
     function (tracedata) {
-        console.log(tracedata);
         ImageTracer.imageToSVG(
             "野獣先輩.png",
             function (strOriSvg) {
-                const strSvg = strOriSvg.replaceAll("rgb(55,45,50)", "hsl(0 100% 50%)") // 髪、服など
-                                        .replaceAll("rgb(115,79,71)", "hsl(30 100% 40%)") // 影
-                                        .replaceAll("rgb(166,114,102)", "hsl(60 100% 50%)") // 皮膚
-                                        .replaceAll("rgb(254,253,253)", "hsl(250 100% 50%)"); // 背景
-                ImageTracer.appendSVGString(strSvg, "svg-container");
                 for (let i = 0; i < 100; i++) {
-                    ImageTracer.appendSVGString(createRandomColorStrSVG(strOriSvg, tracedata.palette), "svg-container");
+                    let strSvg = strOriSvg;
+                    for (const color of tracedata.palette) {
+                        const src = `rgb(${color.r},${color.g},${color.b})`;
+                        const hslObj = rgb2hsl(color.r, color.g, color.b);
+
+                        hslObj.h = randomInt(1, 360);
+                        hslObj.s = 100;
+
+                        if (hslObj.l > 70) {
+                            hslObj.l = randomInt(50, 70);
+                        }
+                        else if (hslObj.l < 30) {
+                            hslObj.l = randomInt(30, 50);
+                        }
+
+                        const dst = `hsl(${hslObj.h} ${hslObj.s}% ${hslObj.l}%)`;
+                        strSvg = strSvg.replaceAll(src, dst);
+                    }
+                    ImageTracer.appendSVGString(strSvg, "svg-container");
                 }
             },
             options
